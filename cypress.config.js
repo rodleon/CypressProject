9a5f53f472f0fc3cd6b6953921beaa297fa36c19
gouba <goubarodrigue@yahoo.fr>
2023-05-31 21:38:06 +0200

    Correction devtools

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        console.log(launchOptions.args);
+
+        if (browser.name == "chrome") {
+          launchOptions.args.push("--disable-gpu");
+        }
+
+        return launchOptions;
+      });
+
+      // implement additional node event listeners here
     },
   },
 });
